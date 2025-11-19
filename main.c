7247ac87be790437ec1b2d964f987fb30302b38f
Ganavi Purushothama <PES2UG24CS167@pesuonline.onmicrosoft.com>
2025-11-19 19:49:55 +0530

    Refactor particle management in main.c
    Updated the particle management system to improve functionality and user interaction.

--- main.c
+++ main.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "particle.h" //including user defined header file 
+#include "particle.h"
 
-// Input helper: Add particle
 void promptAddParticle(ParticleList *list) {
     Particle p;
-
     printf("Enter x y dx dy lifetime:\n");
     if (scanf("%d%d%d%d%d", &p.x, &p.y, &p.dx, &p.dy, &p.lifetime) != 5) {
         printf("Invalid input\n");
         while (getchar() != '\n');
         return;
     }
-
     addParticle(list, p);
     printf("Particle added.\n");
 }
 
-// Input helper: Delete particle by index
 void promptDeleteParticle(ParticleList *list) {
     int index, i = 0;
-    printf("Enter particle index to delete:\n");
-
+    printf("Enter particle index to delete (starting 0):\n");
     if (scanf("%d", &index) != 1) {
         printf("Invalid input\n");
         while (getchar() != '\n');
         return;
     }
-
     ParticleCell *curr = list->head;
-
     while (curr && i < index) {
         curr = curr->next;
         i++;
     }
-
     if (curr) {
         removeParticle(list, curr);
         printf("Particle deleted.\n");
@@ -44,23 +36,14 @@ void promptDeleteParticle(ParticleList *list) {
 }
 
 int main() {
-    ParticleList system;
-    initParticleList(&system);
+    ParticleSystem system;
+    initParticleSystem(&system);
 
     int choice;
     int running = 1;
 
     while (running) {
-        printf("\n==== MENU ====\n");
-        printf("1. Add Particle\n");
-        printf("2. Delete Particle\n");
-        printf("3. Update Particles\n");
-        printf("4. Render Using BST\n");
-        printf("5. Apply Global Force\n");
-        printf("6. Clear All Particles\n");
-        printf("7. Exit\n");
-        printf("Enter choice: ");
-
+        printf("\nMenu:\n1. Add Particle\n2. Delete Particle\n3. Update Particles\n4. Render Animation\n5. Render Using BST\n6. Exit\nEnter choice: ");
         if (scanf("%d", &choice) != 1) {
             printf("Invalid input\n");
             while (getchar() != '\n');
@@ -69,43 +52,30 @@ int main() {
 
         switch (choice) {
             case 1:
-                promptAddParticle(&system);
+                promptAddParticle(&system.list);
                 break;
-
             case 2:
-                promptDeleteParticle(&system);
+                promptDeleteParticle(&system.list);
                 break;
-
             case 3:
                 updateParticles(&system);
+                printf("Particles updated.\n");
                 break;
-
             case 4:
-                renderParticlesUsingBST(&system);
+                renderAnimation(&system);
                 break;
-
-            case 5: {
-                float fx, fy;
-                printf("Enter global forceX forceY: ");
-                scanf("%f %f", &fx, &fy);
-                applyGlobalForce(&system, fx, fy);
+            case 5:
+                renderParticlesUsingBST(&system.list);
                 break;
-            }
-
             case 6:
-                clearAllParticles(&system);
-                break;
-
-            case 7:
                 running = 0;
                 break;
-
             default:
                 printf("Invalid choice\n");
         }
     }
 
-    cleanupParticles(&system);
+    cleanupParticles(&system.list);
     return 0;
 }
 
